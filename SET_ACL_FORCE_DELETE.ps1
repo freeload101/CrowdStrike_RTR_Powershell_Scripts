eb17a2c2ac4d9d733245751037f870eadaab5015
operat0r <freeload101@users.noreply.github.com>
2021-07-14 10:57:29 -0400

    added taskkill and loggoff for locked files

--- SET_ACL_FORCE_DELETE.ps1
+++ SET_ACL_FORCE_DELETE.ps1
@@ -212,6 +212,24 @@ Set-Variable -Name ErrorActionPreference -Value SilentlyContinue
 
 (Get-ChildItem -Path 'c:\'  -Depth 200  -Force -Recurse -Filter "*GeoComply*").Fullname |
 ForEach-Object {
+
+
+echo Killing Browsers
+cmd /c taskkill.exe /F /IM chrome.exe
+cmd /c taskkill.exe /F /IM outlook.exe
+cmd /c taskkill.exe /F /IM IEXPLORE.EXE
+cmd /c taskkill.exe /F /IM msedge.exe
+cmd /c taskkill.exe /F /IM firefox.exe
+
+echo Forcing Loggoff for locked files
+logoff 1
+logoff 2
+logoff 3
+logoff 4
+logoff 5
+logoff 6
+logoff 7
+
 echo "Setting permissions to SYSTEM for $_”
 Set-Owner -Recurse -Account '.\SYSTEM' -Verbose
 
