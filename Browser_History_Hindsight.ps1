b5b0b86c85f803b21ace875097d2ae978d8782d7
Robert McCurdy <freeload101@users.noreply.github.com>
2022-07-21 17:32:03 -0400

    Update Browser_History_Hindsight.ps1

--- Browser_History_Hindsight.ps1
+++ Browser_History_Hindsight.ps1
@@ -2,15 +2,15 @@ Set-Variable -Name ErrorActionPreference -Value SilentlyContinue
 
 
 echo '-------------------------';
-echo "[+] INFO: Fetching All Users Chrome History (MSEdge)"
+echo "[+] INFO: Fetching Latest 4 Users Chrome History (MSEdge)"
 echo '-------------------------';
 New-Item -Path 'C:\windows\Temp\ftech_temp' -ItemType Directory | Out-Null
 Invoke-WebRequest -Uri "https://github.com/obsidianforensics/hindsight/releases/download/v2021.12/hindsight.exe" -OutFile "C:\windows\Temp\ftech_temp\hindsight.exe"
 
-Get-ChildItem -Directory -Path "C:\Users\$_"   -ErrorAction SilentlyContinue -Force |Select-String -Pattern "(All|Default|default|Public|desktop)" -NotMatch | ForEach-Object {
+Get-ChildItem -Directory -Path "C:\Users\$_"   -ErrorAction SilentlyContinue -Force | Sort CreationTime -Descending  | Select-String -Pattern "(All|Default|default|Public|desktop)" -NotMatch |  Select-Object -First 4 | ForEach-Object {
 Echo "[+] INFO: Crawling $_"
 Start-Process -FilePath "C:\windows\Temp\ftech_temp\hindsight.exe" -ArgumentList  "   -i `"c:\Users\$_\AppData\Local`" -o `"C:\windows\Temp\ftech_temp\hindsight_$_`"  " -WorkingDirectory "C:\windows\Temp\ftech_temp\"  -Verbose   -WindowStyle Hidden -Wait 
-}
+} 
 
 Get-ChildItem  C:\windows\Temp\ftech_temp\ -filter *.xlsx |
 Compress-Archive -Destination C:\windows\Temp\ftech_temp\hindsight.zip -Force
