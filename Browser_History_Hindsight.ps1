9cabe85c39e073a908ec6c6bd60394fa530e03da
Robert McCurdy <freeload101@users.noreply.github.com>
2022-07-21 17:13:07 -0400

    Get more history then then RTR IR Script (MSEdge,Chrome)

---
+++ Browser_History_Hindsight.ps1
@@ -0,0 +1,18 @@
+Set-Variable -Name ErrorActionPreference -Value SilentlyContinue
+
+
+echo '-------------------------';
+echo "[+] INFO: Fetching All Users Chrome History (MSEdge)"
+echo '-------------------------';
+New-Item -Path 'C:\windows\Temp\ftech_temp' -ItemType Directory | Out-Null
+Invoke-WebRequest -Uri "https://github.com/obsidianforensics/hindsight/releases/download/v2021.12/hindsight.exe" -OutFile "C:\windows\Temp\ftech_temp\hindsight.exe"
+
+Get-ChildItem -Directory -Path "C:\Users\$_"   -ErrorAction SilentlyContinue -Force |Select-String -Pattern "(All|Default|default|Public|desktop)" -NotMatch | ForEach-Object {
+Echo "[+] INFO: Crawling $_"
+Start-Process -FilePath "C:\windows\Temp\ftech_temp\hindsight.exe" -ArgumentList  "   -i `"c:\Users\$_\AppData\Local`" -o `"C:\windows\Temp\ftech_temp\hindsight_$_`"  " -WorkingDirectory "C:\windows\Temp\ftech_temp\"  -Verbose   -WindowStyle Hidden -Wait 
+}
+
+Get-ChildItem  C:\windows\Temp\ftech_temp\ -filter *.xlsx |
+Compress-Archive -Destination C:\windows\Temp\ftech_temp\hindsight.zip -Force
+
+echo "type: get C:\windows\Temp\ftech_temp\hindsight.zip"
