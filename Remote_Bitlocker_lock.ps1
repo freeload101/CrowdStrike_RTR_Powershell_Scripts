be95420c01c817a527233c181f6d55ec0ee826fd
Robert McCurdy <freeload101@users.noreply.github.com>
2022-08-27 21:51:36 -0400

    Update Remote_Bitlocker_lock.ps1

--- Remote_Bitlocker_lock.ps1
+++ Remote_Bitlocker_lock.ps1
@@ -15,8 +15,8 @@ manage-bde -forcerecovery C:
 # YOU MUST COPY THE KEY (PASSWORD) TO UNLOCK THE DRIVE IF YOU LOSE THE KEY YOU WILL NOT BE ABLE TO RECOVER ANYTHING FROM THE C: DRIVE !!!
 # EXAMPLE 713438-591129-666237-608498-028864-058685-409024-701756
  
- 
+Write-Output "$([regex]::Matches($NewPassword, 'Key\sProtectors\sAdded:(?:.*\n)*?.*ID:\s{(?<ID>[^}]+)}\s*Password:\s*(?<Password>[^\s]+)'))"
  
  
 # force Reboot system to trigger recovery prompt 
-Restart-Computer -Force
+# Restart-Computer -Force
