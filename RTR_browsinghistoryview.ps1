a0986d05a04cbbdd42ce0ab4484794a061ce9a13
Robert McCurdy <freeload101@users.noreply.github.com>
2022-09-16 11:37:35 -0400

    Update RTR_browsinghistoryview.ps1

--- RTR_browsinghistoryview.ps1
+++ RTR_browsinghistoryview.ps1
@@ -6,9 +6,15 @@ Remove-Item -Path "C:\windows\Temp\ftech_temp\report.csv" -Force
 Invoke-WebRequest -Uri "https://www.nirsoft.net/utils/browsinghistoryview-x64.zip" -OutFile "C:\windows\Temp\ftech_temp\browsinghistoryview-x64.zip"
 
 Expand-Archive  "C:\windows\Temp\ftech_temp\browsinghistoryview-x64.zip"  -DestinationPath "C:\windows\Temp\ftech_temp"  -Force
+ 
+echo "[+] INFO: Fetching Latest 6 Users Chrome,Edge History"
 
-$CurrentUser = ((Get-WMIObject -ClassName Win32_ComputerSystem).Username).Split('\')[1]
-echo "ComputerName $env:COMPUTERNAME UserName $CurrentUser "  
-Start-Process -FilePath "C:\windows\Temp\ftech_temp\BrowsingHistoryView.exe" -ArgumentList  " /HistorySource 4 /HistorySourceFolder `"C:\users\$CurrentUser\`"  /VisitTimeFilterType 3 /VisitTimeFilterValue 2 /LoadIE 1 /LoadFirefox 1 /LoadChrome 1 /scomma `"C:\windows\Temp\ftech_temp\report.csv`" /sort `"Visit Time`""  -Wait  -Verbose -WindowStyle Hidden   
-$users = Import-Csv -Path C:\windows\Temp\ftech_temp\report.csv
-echo $users.URL
+Get-ChildItem -Directory -Path "C:\Users\$_"    -ErrorAction SilentlyContinue -Force | Sort LastWriteTime  -Descending  | Select-Object -First 6  |   ForEach-Object  {
+    if (($_).Name -notmatch 'public|default|\$'){
+        echo '-------------------------';
+		echo "[+] INFO: Displaying History for HostName: $env:computername User: $_ MSEdge/Chrome  "
+		echo '-------------------------';
+        Start-Process -FilePath "C:\windows\Temp\ftech_temp\BrowsingHistoryView.exe" -ArgumentList  " /HistorySource 4 /HistorySourceFolder `"C:\users\$_\`"  /VisitTimeFilterType 3 /VisitTimeFilterValue 2 /LoadIE 1 /LoadFirefox 1 /LoadChrome 1 /scomma `"C:\windows\Temp\ftech_temp\report.csv`" /sort `"Visit Time`""  -Wait  -Verbose -WindowStyle Hidden   
+        Import-Csv "C:\windows\Temp\ftech_temp\report.csv" | Select -ExpandProperty  URL |Get-Unique -AsString
+    }
+}
